e98e48bebdf96795e44199e8db787829e8e8ffdf
AvrilWarrior <140816572+AvrilWarrior@users.noreply.github.com>
2024-08-24 15:39:03 +0200

    Add files via upload

---
+++ RegistrationLogin.sql
@@ -0,0 +1,13 @@
+CREATE TABLE register(
+username VARCHAR(200),
+resName VARCHAR(200),
+resSurname VARCHAR(200),
+resPassword VARCHAR(200),
+resConfirmPassword VARCHAR(200),
+email VARCHAR(200)
+);
+
+CREATE TABLE login(
+lUsername VARCHAR(200),
+lPassword VARCHAR(200)
+);
